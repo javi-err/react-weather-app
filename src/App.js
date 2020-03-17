a6eba770373a62a8e97f2020f595d17073523999
Javori Smart <jsmart1993@hotmail.com>
2020-03-17 10:11:29 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Temperature from './Temperature'
+
+
+// eslint-disable-next-line 
+const api = {
+  key: "638e10fa0355fe08af8c6d4076f8da62",
+  baseURL: "https://api.openweathermap.org/data/2.5"
+}
+
+const dateBuilder = (dateget) => {
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+   'September', 'October', 'November', 'December'];
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[dateget.getDay()];
+  let date = dateget.getDate();
+  let month = months[dateget.getMonth()];
+  let year = dateget.getFullYear();
+
+  return `${day} ${month} ${date}, ${year}`
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className = "App">
+      <main>
+        <div className='search-box'>
+          <input type="text" className="search-bar" placeholder="Search cities...">
+          </input>
+        </div>
+
+          <div className="location-bar">
+            <div className="location">New York City</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+
+          <div className="weather-box"> 
+            <Temperature />
+            <div className="weather">Sunny</div>
+          </div>
+        </main>  
+      </div>
+      
+      
   );
 }
 
---
+++ src/Temperature.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class Temperature extends Component {
+
+  
+
+  temperatureConversion = (temp) => {
+  }
+  
+  render() {
+    return (
+    
+      <div className="temp">
+        90°F 
+      </div>
+    
+    )
+  }
+}
+
+export default Temperature
+
